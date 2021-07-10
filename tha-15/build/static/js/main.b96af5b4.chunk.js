2caac9e1976ced330b93a2183ccce980febd79bb
ankkyprasad <58213380+ankkyprasad@users.noreply.github.com>
2021-07-10 22:21:19 +0530

    Delete tha-15/build directory

--- tha-15/build/static/js/main.b96af5b4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptha-15"]=this["webpackJsonptha-15"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),s=n(0);var o=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"Hello World!"})})};c.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.b96af5b4.chunk.js.map
